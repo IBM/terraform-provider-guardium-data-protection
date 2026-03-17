96c15c3a1e9a9afd3cdd35f8a042f25b4b0b5369
MING DONG <dongm@us.ibm.com>
2026-03-17 10:32:24 -0400

    update edge-k3s-e2e-deploy
    Signed-off-by: MING DONG <dongm@us.ibm.com>

--- examples/complete/edge-k3s-e2e-deploy/outputs.tf
+++ examples/complete/edge-k3s-e2e-deploy/outputs.tf
@@ -1,42 +1,27 @@
-# Complete K3S Deployment Outputs - Custom Provider Edition
+# Complete K3S Edge Deployment Outputs
 
 # ============================================================================
-# Fyre VM Outputs (if deployed)
+# Cluster Outputs
 # ============================================================================
 
-output "fyre_cluster_id" {
-  description = "The ID of the created Fyre cluster"
-  value       = var.create_fyre_vm ? guardium-data-protection_fyre_vm.cluster[0].id : null
+output "cluster_name" {
+  description = "Name of the K3S cluster"
+  value       = var.cluster_name
 }
 
-output "fyre_cluster_name" {
-  description = "Name of the Fyre cluster"
-  value       = var.create_fyre_vm ? guardium-data-protection_fyre_vm.cluster[0].cluster_name : var.external_cluster_name
+output "k3s_nodes" {
+  description = "List of K3S node hostnames"
+  value       = var.k3s_nodes
 }
 
-output "fyre_cluster_hostname" {
-  description = "The hostname of the cluster master"
-  value       = var.create_fyre_vm ? guardium-data-protection_fyre_vm.cluster[0].cluster_host_name : null
+output "k3s_master_node" {
+  description = "K3S master node hostname"
+  value       = var.k3s_master_node
 }
 
-output "domain_suffix" {
-  description = "Domain suffix for the cluster"
-  value       = local.domain_suffix
-}
-
-output "master_node_fqdns" {
-  description = "List of master node FQDNs"
-  value       = local.master_node_fqdns
-}
-
-output "worker_node_fqdns" {
-  description = "List of worker node FQDNs"
-  value       = local.worker_node_fqdns
-}
-
-output "all_node_fqdns" {
-  description = "List of all node FQDNs"
-  value       = local.all_node_fqdns
+output "worker_count" {
+  description = "Number of worker nodes"
+  value       = local.worker_count
 }
 
 # ============================================================================
@@ -45,22 +30,22 @@ output "all_node_fqdns" {
 
 output "k3s_version" {
   description = "Installed K3S version"
-  value       = var.install_k3s? guardium-data-protection_k3s_cluster.main[0].k3s_version : null
+  value       = var.install_k3s ? guardium-data-protection_k3s_cluster.main[0].k3s_version : null
 }
 
 output "k3s_cluster_type" {
   description = "K3S cluster type (single-node or multi-node)"
-  value       = var.install_k3s? guardium-data-protection_k3s_cluster.main[0].cluster_type : null
+  value       = var.install_k3s ? guardium-data-protection_k3s_cluster.main[0].cluster_type : null
 }
 
 output "k3s_primary_master" {
   description = "Primary K3S master node"
-  value       = var.install_k3s? guardium-data-protection_k3s_cluster.main[0].primary_master : var.external_k3s_master_node
+  value       = var.install_k3s ? guardium-data-protection_k3s_cluster.main[0].primary_master : var.k3s_master_node
 }
 
 output "kubeconfig_location" {
   description = "Location of kubeconfig on master node"
-  value       = var.install_k3s? guardium-data-protection_k3s_cluster.main[0].kubeconfig_path : "/etc/rancher/k3s/k3s.yaml"
+  value       = var.install_k3s ? guardium-data-protection_k3s_cluster.main[0].kubeconfig_path : "/etc/rancher/k3s/k3s.yaml"
 }
 
 # ============================================================================
@@ -70,13 +55,11 @@ output "kubeconfig_location" {
 output "deployment_summary" {
   description = "Complete deployment summary"
   value = {
-    cluster_name   = var.create_fyre_vm ? guardium-data-protection_fyre_vm.cluster[0].cluster_name : var.external_cluster_name
-    domain_suffix  = local.domain_suffix
-    k3s_version    = var.install_k3s ? guardium-data-protection_k3s_cluster.main[0].k3s_version : null
-    master_count   = length(local.master_node_fqdns)
-    worker_count   = length(local.worker_node_fqdns)
-    total_nodes    = length(local.all_node_fqdns)
-    primary_master = local.primary_master
+    cluster_name   = var.cluster_name
+    k3s_version    = var.install_k3s ? guardium-data-protection_k3s_cluster.main[0].k3s_version : "existing"
+    master_node    = var.k3s_master_node
+    worker_count   = local.worker_count
+    total_nodes    = length(var.k3s_nodes)
   }
 }
 
@@ -86,7 +69,7 @@ output "access_instructions" {
     To access the K3S cluster:
 
     1. SSH to the primary master node:
-       ssh ${var.ssh_user}@${local.primary_master}
+       ssh ${var.ssh_user}@${var.k3s_master_node}
 
     2. Set KUBECONFIG environment variable:
        export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
@@ -99,7 +82,7 @@ output "access_instructions" {
 
 output "quick_access" {
   description = "Quick access command"
-  value       = "ssh ${var.ssh_user}@${local.primary_master}"
+  value       = "ssh ${var.ssh_user}@${var.k3s_master_node}"
 }
 
 # ============================================================================
@@ -108,37 +91,37 @@ output "quick_access" {
 
 output "rook_ceph_installed" {
   description = "Whether Rook-Ceph is installed"
-  value       = var.manage_rook_ceph && var.install_rook_ceph
+  value       = var.install_rook_ceph
 }
 
 output "rook_ceph_version" {
   description = "Installed Rook-Ceph version (if installed)"
-  value       = var.manage_rook_ceph && var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].rook_ceph_version : null
+  value       = var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].rook_ceph_version : null
 }
 
 output "rook_ceph_namespace" {
   description = "Kubernetes namespace where Rook-Ceph is installed"
-  value       = var.manage_rook_ceph && var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].namespace : null
+  value       = var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].namespace : null
 }
 
 output "rook_ceph_cluster_type" {
   description = "Rook-Ceph cluster type: test or production"
-  value       = var.manage_rook_ceph && var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].cluster_type : null
+  value       = var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].cluster_type : null
 }
 
 output "rook_ceph_cephfs_storage_class" {
   description = "CephFS storage class name (if installed)"
-  value       = var.manage_rook_ceph && var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].cephfs_storage_class : null
+  value       = var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].cephfs_storage_class : null
 }
 
 output "rook_ceph_block_storage_class" {
   description = "RBD block storage class name (if installed)"
-  value       = var.manage_rook_ceph && var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].block_storage_class : null
+  value       = var.install_rook_ceph ? guardium-data-protection_rook_ceph_cluster.this[0].block_storage_class : null
 }
 
 output "rook_ceph_summary" {
   description = "Rook-Ceph installation summary (if installed)"
-  value = var.manage_rook_ceph && var.install_rook_ceph ? join("\n", [
+  value = var.install_rook_ceph ? join("\n", [
     "Rook-Ceph Installation Summary:",
     "  Cluster:      ${guardium-data-protection_rook_ceph_cluster.this[0].cluster_name}",
     "  Platform:     ${guardium-data-protection_rook_ceph_cluster.this[0].platform}",
