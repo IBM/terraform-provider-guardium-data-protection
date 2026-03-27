6d2de45fb00880bdef8301dad1365d2603c9d799
MING DONG <dongm@us.ibm.com>
2026-03-27 11:20:16 -0400

    Update provider source to match hashicorp public registry
    Signed-off-by: MING DONG <dongm@us.ibm.com>

--- examples/complete/edge-ocp-e2e-deploy/main.tf
+++ examples/complete/edge-ocp-e2e-deploy/main.tf
@@ -8,11 +8,8 @@
 terraform {
   required_providers {
     guardium-data-protection = {
-      # For internal testing with IBM Artifactory
-      # source  = "registry.terraform.io/ibm/guardium-data-protection"
-      # For public release (uncomment when published to HashiCorp registry)
-      source  = "hashicorp.com/ibm/guardium-data-protection"
-      version = "~>1.3.8"
+      source  = "IBM/guardium-data-protection"
+      version = "> 1.4.0"
     }
   }
 }
